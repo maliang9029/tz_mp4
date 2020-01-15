b91cb0b274f8e92eeafaa6656bafe4d72cd25b0d
[maliang45] <[maliang8@jd.com]>
2020-01-15 11:14:54 +0800

    new profiles

---
+++ tz_mp4/VideoRecordMan.cpp
@@ -0,0 +1,14 @@
+#include "VideoRecordMan.h"
+
+CVideoRecordMan::CVideoRecordMan(const char* sFilePath, unsigned int w,unsigned int h,unsigned int frameRate)
+{
+}
+
+CVideoRecordMan::~CVideoRecordMan(void)
+{
+}
+
+bool CVideoRecordMan::play_start(unsigned int hWnd)
+{
+    return true;
+}
